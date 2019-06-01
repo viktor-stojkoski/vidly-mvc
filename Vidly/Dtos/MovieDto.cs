9fe949400c41864911b8fb643ecb67d56a3c80bd
Viktor Stojkoski <viktor.98@hotmail.com>
2019-06-01 17:15:06 +0200

    Section 6 Exercise Finished - Added API for Movies

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        [Required]
+        public int NumberInStock { get; set; }
+    }
+}
